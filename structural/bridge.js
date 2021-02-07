2313faeee533cdc1d64cf40e9a1d3ff2fb468f94
Vecktor <v3ckt0r.tg@gmail.com>
2021-02-08 00:06:49 +0200

    added tests to bridge pattern

--- structural/bridge.js
+++ structural/bridge.js
@@ -8,26 +8,76 @@ class DocumentCoderInterface {
 
 class DocumentCoder extends DocumentCoderInterface {
     encode() {
+        console.log("Getting data");
+        console.log("Checking data");
         console.log("Encode document");
+        console.log("Sending result\n");
     }
 
     decode() {
+        console.log("Getting data");
         console.log("Decode document");
+        console.log("Checking result\n");
     }
 
     getString(number) {
+        console.log("Opening document");
+        console.log("Starting reading document");
+        console.log("Go to string number " + number);
+        console.log("Returning result\n");
         return `String number: ${number}`;
     }
 
-    setString() {
-        console.log(`String number: ${number} was changed to`);
+    setString(number, str) {
+        console.log("Opening document");
+        console.log("Starting reading document");
+        console.log("Go to string number " + number);
+        console.log("Replacing string by new value");
+        console.log(`String number ${number} was changed to "${str}"\n`);
     }
 
     archive() {
-        console.log("Document was archived");
+        console.log("Getting data");
+        console.log("Archiving document\n");
     }
 }
 
 class DocumentViewer {
-    constructor()
+    constructor(coder) {
+        this.coder = coder;
+    }
+
+    showDocument() {
+        console.log("Showing document\n");
+    }
+
+    encode() {
+        this.coder.encode();
+    }
+
+    decode() {
+        this.coder.decode();
+    }
+
+    getString(number) {
+        this.coder.getString(number);
+    }
+
+    setString(number, str) {
+        this.coder.setString(number, str);
+    }
+
+    archive() {
+        this.coder.archive();
+    }
 }
+
+let documentCoder = new DocumentCoder();
+let documentViewer = new DocumentViewer(documentCoder);
+
+documentViewer.decode();
+documentViewer.encode();
+documentViewer.getString(55);
+documentViewer.setString(31, "new string");
+documentViewer.archive();
+documentViewer.showDocument();
